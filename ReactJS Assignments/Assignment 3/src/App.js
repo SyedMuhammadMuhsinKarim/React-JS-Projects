37002cd73e199284fe744a52c8c5317491412cfe
Syed Muhammad Muhsin karim <37833481+SyedMuhammadMuhsinKarim@users.noreply.github.com>
2019-01-08 20:40:16 +0500

    Update App.js

--- ReactJS Assignments/Assignment 3/src/App.js
+++ ReactJS Assignments/Assignment 3/src/App.js
@@ -10,6 +10,10 @@ const list = [
   { id: "5", employee_name: "kumar", employee_salary: "3", employee_age: "28" },
   { id: "6", employee_name: "kumar", employee_salary: "3", employee_age: "29", }
 ];
+
+const isSearched = searchTerm => item =>
+    item.employee_name.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -23,6 +27,7 @@ class App extends Component {
       EmployeeName: '',
       EmployeeSalary: '',
       EmployeeAge: '',
+      searchTerm: "",
     };
 
     this.getEmail = this.getEmail.bind(this);
@@ -34,6 +39,9 @@ class App extends Component {
     this.getEmployeeAge = this.getEmployeeAge.bind(this);
     this.add = this.add.bind(this);
     this.update = this.update.bind(this);
+    this.cancel = this.cancel.bind(this);
+    // this.isSearched = this.isSearched.bind(this);
+    this.onSearch = this.onSearch.bind(this);
   }
 
   getEmail(event) {
@@ -46,7 +54,7 @@ class App extends Component {
 
   onLogin(event) {
     if (!this.state.email && !this.state.password) {
-      swal("Someething Wrong", "Bahiya Form ki Entry Khali Chor di hai, dobarah fill karo", "error")
+      swal("آپکا اندراج مکمل نہیں", " آپ نے پاسورڈ یاای  نہں لکھا تمام اندراج مکمل کریں ", "error")
     } else {
       if (this.state.email === "admin@example.com" && this.state.password === "admin") {
         this.setState({ login: true });
@@ -91,8 +99,6 @@ class App extends Component {
   onEdit(id) {
     const { list } = this.state;
 
-    console.log('mien yaha hoon')
-
     this.setState({ EmployeeName: list[id].employee_name, EmployeeSalary: list[id].employee_salary, EmployeeAge: list[id].employee_age, current_index: id })
     console.log(this.state.current_index)
   }
@@ -108,47 +114,55 @@ class App extends Component {
 
   cancel() {
     this.setState({ current_index: list.length });
+    // event.preventDefault();
   }
 
+  onSearch = event =>
+    this.setState({ searchTerm: event.target.value });
+
   render() {
-    const { email, login, list } = this.state;
+    const { login, list } = this.state;
     return (
       <div className="App">
         {!login && <form>
-          <input type="email" placeholder="Type Your Email" onChange={this.getEmail} />
-          <input type="password" placeholder="Type Your Password" onChange={this.getPass} />
-          <input type="submit" onClick={this.onLogin} value="Login" />
+          <input type="email" placeholder="اپنا ای میل لکھیں" onChange={this.getEmail} />
+          <input type="password" placeholder="پاسورڈ یہاں لکھیں" onChange={this.getPass} />
+          <input type="submit" onClick={this.onLogin} value="لاگ ان کریں" />
         </form>}
 
-        {!login && <div>
-          <p>Employ Data</p>
+        {login && <div>
+          <p>ملازمین کا ڈیٹابیس</p>
           <form>
-            <input type="text" placeholder="Employee Name" onChange={this.getEmployeeName} value={this.state.EmployeeName} />
-            <input type="text" placeholder="Salary" onChange={this.getEmployeeSalary} value={this.state.EmployeeSalary} />
-            <input type="text" placeholder="age" onChange={this.getEmployeeAge} value={this.state.EmployeeAge} />
+            <input type="text" placeholder="ملازم کا نام" onChange={this.getEmployeeName} value={this.state.EmployeeName} />
+            <input type="number" placeholder="ملازم کی تنخواہ" onChange={this.getEmployeeSalary} value={this.state.EmployeeSalary} />
+            <input type="number" placeholder="ملازم کی عمر" onChange={this.getEmployeeAge} value={this.state.EmployeeAge} />
             {this.state.current_index === this.state.list.length
               ?
-              <input type="submit" onClick={this.add} value="Add" />
+              <input type="submit" onClick={this.add} value="شامل کریں" />
               :
               <div>
-                <input type="submit" onClick={this.update} value="Udate" />
-                <input type="submit" onClick={() => this.cancel.bind(this)} value="Cancel" />
+                <input type="submit" onClick={this.update} value="اپڈیٹ" />
+                <input type="submit" onClick={this.cancel} value="منسوخ" />
               </div>
             }
           </form>
 
-          <table className="mx-auto" style={{ width: 1000 }} border="1">
+          <form>
+            <input type="text" placeholder="ملازم کو تلاش کریں" onChange={this.onSearch} value={this.state.searchTerm} />
+          </form>
+
+          <table className="mx-auto responsive" border="1">
             <tbody>
               <tr>
-                <th>S.no</th>
+                <th>اندراج</th>
                 {/* <th>id</th> */}
-                <th>name</th>
-                <th>salary</th>
-                <th>age</th>
-                <th>Edit</th>
-                <th>Del</th>
+                <th>نام</th>
+                <th>تنخواہ</th>
+                <th>عمر</th>
+                <th>ایڈیٹ</th>
+                <th>مٹاو</th>
               </tr>
-              {list.map((item, index) => {
+              {list.filter(isSearched(this.state.searchTerm)).map((item, index) => {
                 return (
                   <tr key={index}>
                     <td>{index + 1}</td>
@@ -156,8 +170,8 @@ class App extends Component {
                     <td>{item.employee_name}</td>
                     <td>{item.employee_salary}</td>
                     <td>{item.employee_age}</td>
-                    <td><button onClick={this.onEdit.bind(this, index)}>Edit</button></td>
-                    <td><button onClick={() => this.onDismiss(item.id)}>Delete</button></td>
+                    <td><button onClick={this.onEdit.bind(this, index)}>ایڈیٹ کریں</button></td>
+                    <td><button onClick={() => this.onDismiss(item.id)}>مٹائیں</button></td>
                   </tr>
                 );
               })}</tbody>
