5a235a21f4ab0f746160d018221930492694561e
Syed Muhammad Muhsin karim <37833481+SyedMuhammadMuhsinKarim@users.noreply.github.com>
2019-01-17 23:15:23 +0500

    Update Firebase.js

--- Firebase/Authentication 01/src/Config/Firebase.js
+++ Firebase/Authentication 01/src/Config/Firebase.js
@@ -3,14 +3,5 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyA2NCPHMZMLONSvHCBlKsiTcqIqLjQ6e5E",
-    authDomain: "react-quiz-app-f1d15.firebaseapp.com",
-    databaseURL: "https://react-quiz-app-f1d15.firebaseio.com",
-    projectId: "react-quiz-app-f1d15",
-    storageBucket: "react-quiz-app-f1d15.appspot.com",
-    messagingSenderId: "440044887479"
-  };
-  firebase.initializeApp(config);
 
 export default firebase;
